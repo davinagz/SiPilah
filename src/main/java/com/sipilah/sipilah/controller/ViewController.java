cea02bbc8e33ab03eeaa2df26c2bce3e18ca2a37
unknown <davinagiz06@gmail.com>
2026-05-13 17:15:15 +0700

    mindahin file html semua role ke templates dan update viewcontroller

--- src/main/java/com/sipilah/sipilah/controller/ViewController.java
+++ src/main/java/com/sipilah/sipilah/controller/ViewController.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
+    // 1. Halaman Utama
     @GetMapping("/")
     public String home() {
-        return "redirect:/index.html";
+        return "index"; // Dia akan otomatis mencari templates/index.html
+    }
+
+    // 2. Halaman Pengguna
+    @GetMapping("/pengguna")
+    public String halamanPengguna() {
+        return "pengguna"; // Dia akan mencari templates/pengguna.html
+    }
+
+    // 3. Halaman Petugas
+    @GetMapping("/petugas")
+    public String halamanPetugas() {
+        return "petugas"; // Dia akan mencari templates/petugas.html
     }
 }
